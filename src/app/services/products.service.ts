3baa147ec3415b67f24f02ce17fd6aced7cf7c55
adribasterra <adrianabasterraa@gmail.com>
2021-03-13 16:16:49 +0100

    fin

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -20,14 +20,14 @@ export class ProductsService {
     return new Promise( ( resolve, reject ) => {
       this.http.get("https://portfolio-angular-43274-default-rtdb.firebaseio.com/products_idx.json")
       .subscribe( ( response: Product[] ) => {
-        console.log(response);
         this.products = response;
-  
-        setTimeout( () => {
-          this.loading = false;
-        }, 2000);
+        this.loading = false;
 
-        resolve(void);
+        // setTimeout( () => {
+        //   this.loading = false;
+        // }, 2000);
+
+        resolve;
       })
     })
   }
@@ -52,17 +52,12 @@ export class ProductsService {
   }
 
   private applyFilter( input : string ){
-    this.productsFiltered = this.products.filter( product => {
-      return true;
-    })
-
     this.products.forEach( product => {
-
       const tituloLowCase = product.titulo.toLowerCase();
 
-      if( product.categoria.indexOf(input) >= 0 || product.titulo.indexOf(input) >= 0 ){
+      if( product.categoria.indexOf(input) >= 0 || tituloLowCase.indexOf(input) >= 0 ){
         this.productsFiltered.push(product);
       }
-    })
+    });
   }
 }
